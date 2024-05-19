a3b9df0a3c78bd44bb1627b6fc550b4302d191eb
Maki <sunwood.ai.labs@gmail.com>
2024-05-19 14:42:55 +0900

    🔤 [feat] 文字列操作関数の実装
    - 文字列を逆順にするreverse_string関数と、文字列内の母音の数をカウントするcount_vowels関数を追加。これにより、基本的な文字列処理が可能になります。

--- text_processing/string_operations.py
+++ text_processing/string_operations.py
@@ -9,3 +9,15 @@ def count_vowels(s):
     """文字列内の母音の数をカウント"""
     vowels = 'aeiouAEIOU'
     return sum(1 for char in s if char in vowels)
+
+# string_operations.py
+# 文字列を操作する簡単な関数を定義
+
+def reverse_string(s):
+    """与えられた文字列を逆順にする"""
+    return s[::-1]
+
+def count_vowels(s):
+    """文字列内の母音の数をカウント"""
+    vowels = 'aeiouAEIOU'
+    return sum(1 for char in s if char in vowels)
