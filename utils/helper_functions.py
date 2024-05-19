7976dc26834603d3594cdb8b39fc3ea84545f94b
Maki <sunwood.ai.labs@gmail.com>
2024-05-19 16:06:00 +0900

    bbb

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -88,8 +88,3 @@ def print_details():
         
 # helper_functions.py
 # 便利なヘルパー関数
-
-def print_details():
-    """プログラムの詳細を印刷"""
-    print("This program provides basic string operations and matrix calculations.")
-    
